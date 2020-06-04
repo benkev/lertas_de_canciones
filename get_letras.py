db35237b1084bcf201325959f1e0c86951089514
benkev <lbenkev@gmail.com>
2020-06-04 13:27:15 -0400

    ...

---
+++ get_letras.py
@@ -0,0 +1,15 @@
+import urllib
+import re
+
+#
+# Spanish (code 42) songs, sorted in descending popularity
+#
+ur = urllib.urlopen('https://lyricstranslate.com/en/songs/42/none/none?order=Popularity&sort=desc')
+
+s1 = ur.read()
+
+
+
+
+
+
